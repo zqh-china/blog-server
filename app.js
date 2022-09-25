22214e7b3b3d6f47b521c106363c0ce440de24aa
张启浩 <69977419+zqh-china@users.noreply.github.com>
2022-09-25 21:49:47 +0800

    允许跨域

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const uploadRouter = require('./routers/UploadRouter');
 
 app.use(cors());
 app.use(function(req, res, next) {
-    // res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
     res.header("Access-Control-Allow-Methods", "DELETE, GET, POST, PUT, OPTIONS");
     if (req.method === "OPTIONS") {
